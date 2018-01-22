875f8e21ecbb1706dcfdd385234497f3c492b3d9
andre <aharris1727@gmail.com>
2018-01-22 06:47:34 -0500

    last commit

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-// var box3 =document.getElementsByTagName('seconds');
+
 
 function time() {
-	var date = new Date();
-	var min = date.getMinutes();
-	let sec = date.getSeconds(),
-	    hr = date.getHours();
-	    if(sec < 10){
-	    	return "0" + min;
-	    }
-}
-setInterval(function() {
-	time();
-},1000);
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  
 
-document.getElementById('seconds').InnerHTML=sec.time();
+  
+  
+ 
+  var color = "" + s + m + h;
+  document.getElementById('se').innerHTML = + s;
+  document.getElementById('mi').innerHTML = + m;
+  document.getElementById('ho').innerHTML = + h;
+  document.body.style.background = "#" + color;
+  
+  setTimeout(function() {
+    time()
+  }, 500);
+}
+time();
+
